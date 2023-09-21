86c8f9d8b01ff942403d5cfe79af921f929c572a
Pratiek Parashar <prateek.parashar730@gmail.com>
2023-09-21 13:29:28 +0530

    Added H1 and H2 for SEO purposes

--- client/src/components/about-me/about-me.component.jsx
+++ client/src/components/about-me/about-me.component.jsx
@@ -11,13 +11,13 @@ function AboutMe() {
                 <img src={image} alt="" className='about-image'/>
             </div>
             <div className="about-left-pane">
-                <div className="about-left-head">Pratiek Parashar</div>
-                <div className="about-left-sub">Web Developer</div>
+                <h1 className="about-left-head">Pratiek Parashar</h1>
+                <h2 className="about-left-sub">Web Developer</h2>
                 {/* <input type="button" value="Read My Blog" className='btn btn-primary about-blog-button'/> */}
                 <div className="about-left-data">
                 I am a passionate coder by day and an explorer of the Digital Frontier by night. As a dedicated full stack web developer, I thrive on translating imaginative concepts into functional digital experiences. My relentless drive for innovation is matched by my commitment to refining user experiences and turning clients' visions into reality.
                 <br/><br/>
-                As a backend developer, I specialize in the MERN (MongoDB, Express.js, React, Node.js) stack. This comprehensive expertise allows me to architect databases, optimize back-end processes, and create seamless connections between different components of web applications.
+                As a backend developer, I specialize in the MERN (MongoDB, Express.js, React.js, Node.js) stack. This comprehensive expertise allows me to architect databases, optimize back-end processes, and create seamless connections between different components of web applications.
                 <br/><br/>
                 When I'm not immersed in crafting elegant code and solving complex challenges, you'll find me actively collaborating on projects that span the entire stack. Whether I'm architecting databases, creating dynamic user interfaces, or optimizing back-end processes, I am truly in my element.
                 <br/><br/>
